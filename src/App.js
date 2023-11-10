084c877ae3f1771c735c8a9bdc41f8c30d8660f3
nzerefdragneel <nguyentranngocsuong94@gmail.com>
2023-11-10 15:52:52 +0700

    init homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import AuthService from "./services/auth.service";
+import { Routes, Route, Link } from "react-router-dom";
+
+import Login from "./components/login.component";
+import Home from "./components/home.component";
+class App extends Component {
+  render() {
+    return (
+      <div className="container mt-3">
+      <Routes>
+      <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+
+      </Routes>
     </div>
-  );
+    )
+  }
+  
 }
 
 export default App;
---
+++ src/components/home.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { withRouter } from '../common/with-router';
+class Home extends Component {
+render() {
+  return (
+    <div>
+        <h1> Homepage</h1>
+    </div>
+  )
+}
+}
+export default withRouter(Home);
+
