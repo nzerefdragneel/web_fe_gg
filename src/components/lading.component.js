ddd47a66516f8b4f9b7e48de39660503bbb2eaf6
20120519 <lieunguyenthi1802@gmail.com>
2023-11-14 12:55:31 +0700

    header

--- src/components/lading.component.js
+++ src/components/lading.component.js
@@ -5,23 +5,23 @@ function Lading() {
     return (
         <div className="flex flex-col h-48 w-full">
             <div className='pt-3 pb-4 px-32 flex flex-row justify-between border-b mb-2'>
-                <div className='flex flex-row text-lg'>
-                    <img src='../../public/logo.png' className='w-8 h-8' alt='logo'/>
-                    <div className='px-6 py-2.5 rounded-lg hover:bg-medium-green'>
+                <div className='flex flex-row text-lg items-center'>
+                    <img src='./logo.png' className='w-10 h-10 mr-2' alt='logo'/>
+                    <div className='px-6 py-2.5 mr-2 rounded-lg hover:bg-medium-green hover:cursor-pointer hover:ease-linear duration-300'>
                         Home
                     </div>
-                    <div className='px-6 py-2.5 rounded-lg hover:bg-medium-green'>
+                    <div className='px-6 py-2.5 mr-2 rounded-lg hover:bg-medium-green hover:cursor-pointer hover:ease-linear duration-300'>
                         About Us
                     </div>
-                    <div className='px-6 py-2.5 rounded-lg hover:bg-medium-green'>
+                    <div className='px-6 py-2.5 mr-2 rounded-lg hover:bg-medium-green hover:cursor-pointer hover:ease-linear duration-300'>
                         Contact
                     </div>
                 </div>
                 <div className='flex flex-row gap-2 text-lg'>
-                    <div className='px-6 py-2.5 rounded-lg hover:bg-medium-green'>
+                    <div className='px-6 py-2.5 rounded-lg hover:bg-medium-green hover:cursor-pointer hover:ease-linear duration-300'>
                         Sign up
                     </div>
-                    <div className='px-6 py-2.5 rounded-lg hover:bg-medium-green'>
+                    <div className='px-6 py-2.5 rounded-lg hover:bg-medium-green hover:cursor-pointer hover:ease-linear duration-300'>
                         Log in
                     </div>
                 </div>
