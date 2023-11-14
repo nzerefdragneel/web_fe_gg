54be6ba4299c1e56cb54cf1de0bd8a9490357ffb
unknown <pqthai2502@gmail.com>
2023-11-14 17:26:46 +0700

    push branch

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -85,12 +85,7 @@ class Login extends Component {
     return (
       <div className="col-md-12">
         <div className="card card-container">
-          <img
-            src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-            alt="profile-img"
-            className="profile-img-card"
-          />
-
+        
           <Form
             onSubmit={this.handleLogin}
             ref={c => {
