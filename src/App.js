2b4f7fba9f0b8524058d0c0d9a1a2bccbb4675c0
nzerefdragneel <nguyentranngocsuong94@gmail.com>
2023-11-16 18:03:33 +0700

    edit user

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ import { Routes, Route, Link } from "react-router-dom";
 import Login from "./components/login.component";
 import Home from "./components/home.component";
 import EditUser from "./components/edituser.component";
+import Profile from "./components/profile.component";
 class App extends Component {
   render() {
     return (
-      <div className="container mt-3">
+      <div className="container mt-3 bg-slate-50">
       <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/login" element={<Login/>} />
       <Route path="/edituser" element={<EditUser/>} />
-      
+      <Route path="/lading" element={<Login/>} />
+      <Route path="/profile" element={<Profile/>} />
       </Routes>
     </div>
     )
