747055e68890520c267fcccbf43eff244cc89b8d
thaipq <pqthai2502@gmail.com>
2023-12-11 21:59:36 +0700

    fix invitation link bugs

--- src/components/invitation.component.js
+++ src/components/invitation.component.js
@@ -1,62 +1,52 @@
-import React, { Component } from "react";
-import { Routes, Route, Link, Navigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, Link, Navigate, useNavigate } from "react-router-dom";
 import classService from "../services/class.service";
 
-export default class Invitation extends Component {
-  render() {
-    let params = new URL(window.location).searchParams;
-    const classId = params.get("id");
-    const isTeacher = params.get("isTeacher");
+export default function Invitation() {
+  let params = new URL(window.location).searchParams;
+  const classId = params.get("id");
+  const isTeacher = params.get("isTeacher");
 
-    const user = localStorage.getItem("user");
+  const user = localStorage.getItem("user");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
     if (user === null) {
-      return (
-        <Navigate
-          replace
-          to={`/login?invitationId=${
-            classId !== null ? classId.toString() : "null"
-          }&isTeacher=${isTeacher !== null ? isTeacher.toString() : "null"}`}
-        />
+      navigate(
+        `/login?invitationId=${
+          classId !== null ? classId.toString() : "null"
+        }&isTeacher=${isTeacher !== null ? isTeacher.toString() : "null"}`
       );
     } else {
       const userId = JSON.parse(user).id;
       classService.acceptInvitation(classId, isTeacher, userId).then(
         (response) => {
-          console.log(response.data.status);
-          if (response.data.status === "success") {
-            return <Navigate replace to="/home" />;
+          if (response.status === 200) {
+            console.log("success");
+            navigate(`/home`);
           } else {
-            return (
-              <Navigate
-                replace
-                to={`/login?invitationId=${
-                  classId !== null ? classId.toString() : "null"
-                }&isTeacher=${
-                  isTeacher !== null ? isTeacher.toString() : "null"
-                }`}
-              />
+            navigate(
+              `/login?invitationId=${
+                classId !== null ? classId.toString() : "null"
+              }&isTeacher=${isTeacher !== null ? isTeacher.toString() : "null"}`
             );
           }
         },
         (error) => {
-          return (
-            <Navigate
-              replace
-              to={`/login?invitationId=${
-                classId !== null ? classId.toString() : "null"
-              }&isTeacher=${
-                isTeacher !== null ? isTeacher.toString() : "null"
-              }`}
-            />
+          navigate(
+            `/login?invitationId=${
+              classId !== null ? classId.toString() : "null"
+            }&isTeacher=${isTeacher !== null ? isTeacher.toString() : "null"}`
           );
         }
       );
     }
+  }, [classId, isTeacher, user]);
 
-    return (
-      <>
-        <div className=" "></div>
-      </>
-    );
-  }
+  return (
+    <>
+      <div className=" "></div>
+    </>
+  );
 }
